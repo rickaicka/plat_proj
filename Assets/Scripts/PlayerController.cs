fcbfe949605c5257ae04d86d4726e7b9b2384b6a
Ricardo Salim <ricardosalim7@hotmail.com>
2025-03-30 19:36:52 -0300

    attack animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,11 +19,14 @@ public class PlayerController : MonoBehaviour
     
     Animator animator;
     
+    Rigidbody rb;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -31,6 +34,7 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         GetJumpInput();
+        GetMouseInput();
     }
 
     void Move()
@@ -82,6 +86,55 @@ public class PlayerController : MonoBehaviour
         }
     }
     
+    void GetMouseInput()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(animator.GetBool("isJumpUp"))
+            {
+                StartCoroutine(StopJump());
+            }
+
+            if (animator.GetBool("isRunningBack") || animator.GetBool("isRunning"))
+            {
+                StartCoroutine(StopWalk());
+                StartCoroutine(Attack());
+            }
+            else
+            {
+                StartCoroutine(Attack());
+            }
+        }
+    }
+
+    IEnumerator JumpUp()
+    {
+        speed = 2;
+        var dividedSpeed = ((speed / 2) / 2);
+        
+        yield return new WaitForSeconds(.05f);
+        animator.SetInteger("transition", 2);
+        animator.SetBool("isJumpUp", true);
+        moveDirection = (Vector3.up * 6) + (Vector3.forward * dividedSpeed);
+        //moveDirection.y = 15;
+        //rb.AddForce(Vector3.up * dividedSpeed, ForceMode.VelocityChange);
+        moveDirection = transform.TransformDirection(moveDirection);
+        controller.Move(moveDirection * Time.deltaTime);
+        rotation += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rotation, 0);
+        StartCoroutine(StopJump());
+    }
+
+    IEnumerator Attack()
+    {
+        animator.SetBool("isAttacking", true);
+        animator.SetInteger("transition", 4);
+        yield return new WaitForSeconds(1.8f);
+        animator.SetBool("isAttacking", false);
+        animator.SetBool("isAttackingIdle", true);
+        animator.SetInteger("transition", 5);
+    }
+    
     IEnumerator StopWalk()
     {
         yield return new WaitForSeconds(.2f);
@@ -99,19 +152,6 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isJumpUp", false);
         moveDirection = Vector3.zero * speed;
         moveDirection = transform.TransformDirection(moveDirection);
-    }
-
-    IEnumerator JumpUp()
-    {
-        yield return new WaitForSeconds(.05f);
-        animator.SetInteger("transition", 2);
-        animator.SetBool("isJumpUp", true);
-        moveDirection.y = 30;
-        moveDirection = Vector3.forward * (speed / 2);
-        moveDirection = transform.TransformDirection(moveDirection);
-        controller.Move(moveDirection * Time.deltaTime);
-        rotation += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-        transform.eulerAngles = new Vector3(0, rotation, 0);
-        StartCoroutine(StopJump());
+        speed = 5;
     }
 }
